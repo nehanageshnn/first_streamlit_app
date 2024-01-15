533f1a96348e37dbdd29b8c2424a1ea441d3dedd
nehanageshnn <156354875+nehanageshnn@users.noreply.github.com>
2024-01-15 11:17:17 -0800

    Update streamlit_app.py
    missing "

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/ + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 # write your own comment -what does the next line do? 
